596d045725849b87498f7dd31375d1a5e22b5453
Andrej Makarenko <andrej@brandmen.local>
2026-05-25 16:04:54 +0700

    feat: integrate Android player source code into the main repository

--- android-player/src/main/java/com/brandmen/ads/BootReceiver.java
+++ android-player/src/main/java/com/brandmen/ads/BootReceiver.java
@@ -7,9 +7,7 @@ import android.content.Intent;
 public class BootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        String action = intent.getAction();
-        if ("android.intent.action.BOOT_COMPLETED".equals(action)
-                || "android.intent.action.QUICKBOOT_POWERON".equals(action)) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
             Intent i = new Intent(context, MainActivity.class);
             i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(i);
